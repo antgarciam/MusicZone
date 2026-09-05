3556eb6f6b08929598592f8025a2eb177cbde610
Anthuan <ant.garciam@duocuc.cl>
2026-09-04 23:08:58 -0400

    Implementación de 'nosotros', productos destacados de la tienda, se agregó un footer y conexiones entre inicio con el html de nonosotros

---
+++ inicio.js
@@ -0,0 +1,23 @@
+let productos = [
+    {
+        id: 1,
+        nombre: "Guitarra eléctrica",
+        precio: 249990,
+        descripcion: "Guitarra eléctrica ideal para comenzar a tocar",
+        imagen: "img/guitarra.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Batería",
+        precio: 599990,
+        descripcion: "Batería acústica de 5 piezas",
+        imagen: "img/bateria.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Teclado",
+        precio: 329990,
+        descripcion: "Teclado electrónico de 61 teclas",
+        imagen: "img/teclado.jpg"
+    }
+];
